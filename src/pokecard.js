d9465c65605dccfde4db94edd4eca150d47ffb95
Michael Vanderloon <jenkins92886@gmail.com>
2020-05-13 23:39:26 -0400

    react code along practice

---
+++ src/pokecard.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./pokecard.css";
+const Poke_Api = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+let paddedNum = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+
+class Pokecard extends Component {
+  render() {
+    let imgSrc = `${Poke_Api}${paddedNum(this.props.id)}.png`;
+
+    return (
+      <div className="Pokecard">
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <img src={imgSrc} alt={this.props.name} />
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div  className="Pokecard-data">Exp: {this.props.exp}</div>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
